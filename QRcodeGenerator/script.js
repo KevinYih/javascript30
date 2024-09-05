248682ea3bb566981433b971a255ae0e93fa5ca0
keviny <kevinyih2022@gmail.com>
2024-09-05 00:27:50 -0700

    update shake animation

--- QRcodeGenerator/script.js
+++ QRcodeGenerator/script.js
@@ -5,10 +5,17 @@ const qrUrl = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
 const genBtn = document.getElementById("genBtn");
 
 function generateQR() {
-  qrImage.src =
-    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-    qrText.value;
-  imgBox.classList.add("show-img");
+  if (qrText.value.length > 0) {
+    qrImage.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrText.value;
+    imgBox.classList.add("show-img");
+  } else {
+    qrText.classList.add("error");
+    setTimeout(() => {
+      qrText.classList.remove("error");
+    }, 1000);
+  }
 }
 
 genBtn.addEventListener("click", () => {
